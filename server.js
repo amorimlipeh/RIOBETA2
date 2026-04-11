9b0a8330047f0650ac19a80384e5a4d1d7891bf6
amorimlipeh <am.felipe1994@gmail.com>
2026-04-11 13:39:32 -0300

    fix porta railway

--- server.js
+++ server.js
@@ -84,4 +84,8 @@ app.post("/api/estoque/movimentar", (req, res) => {
   res.json({ ok: true });
 });
 
-app.listen(3000, () => console.log("Servidor rodando 🚀"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
